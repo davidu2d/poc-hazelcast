a5321d86e4c7f43842d87ede032dde9fc6a58922
davidu2d <u2dtecnologia@gmail.com>
2022-11-26 14:24:33 -0300

    correção na HazelcastConfig

--- src/main/java/com/u2d/hazelcast/config/HazelcastConfig.java
+++ src/main/java/com/u2d/hazelcast/config/HazelcastConfig.java
@@ -3,11 +3,13 @@ package com.u2d.hazelcast.config;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HazelcastConfig {
 
+    @Bean
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
